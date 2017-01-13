545970cd1b2b694846511f21620ab35325ee35e1
kishan <kishan@30191023JOY205>
2017-01-12 16:35:47 -0800

    Initial Commit

---
+++ contoso02/contoso02/DAL/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using contoso02.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace contoso02.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ contoso02/contoso02/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace contoso02.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstMidName { get; set; }
+        public String LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
